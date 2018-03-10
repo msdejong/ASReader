7e4b8597ff2eb65696dd7ba0704c8702f6829e4e
Michiel29 <michiel.s.dejong@gmail.com>
2018-03-10 02:00:12 -0500

    update to dataloader, not functional yet but closer

--- dataloaders/data.py
+++ dataloaders/data.py
@@ -1,9 +1,8 @@
 
 
 class Data:
-    def __init__(self, document_tokens, query_tokens, answer_token, entity_to_index, index_to_entity):
+    def __init__(self, document_tokens, query_tokens, answer_token, entities):
         self.document_tokens = document_tokens
         self.query_tokens = query_tokens
         self.answer_token = answer_token
-        self.entity_to_index = entity_to_index
-        self.index_to_entity = index_to_entity
+        self.entities = entities
--- dataloaders/dataloader.py
+++ dataloaders/dataloader.py
@@ -28,44 +28,72 @@ class DataLoader():
 
                 document_tokens = lines[2].split()
                 query_tokens = lines[4].split()
-                answer = lines[6]
-
-                index_to_entity = {}
-                entity_to_index = {}
+                answer_token = lines[6]
+                entities = []
 
                 for line in lines[8:]:
-                    try:
-                        index, entity = line.strip('\n').split(":", 1)
-                    except:
-                        print(line)
-                        print(line.strip('\n').split(":"))
-                    index_to_entity[index] = entity
-                    entity_to_index[entity] = index
-
-                data_point = Data(document_tokens, query_tokens, answer, entity_to_index, index_to_entity)
+                    index, entity = line.strip('\n').split(":", 1)
+                    entities.append(entity)
+            
+                data_point = Data(document_tokens, query_tokens, answer_token, entities)
                 data.append(data_point)    
 
         with open(output_path, "wb") as fout:
             pickle.dump(data, fout)
 
 
+    def generate_vocabulary(self, data, special_tokens = ["<unk>"]):
 
-    def process_dataset(self, input_directory, output_directory):
+        word_set = set()
 
-        self.process_data(os.path.join(input_directory, "training"), os.path.join(output_directory, "training.pickle"))
-        self.process_data(os.path.join(input_directory, "validation"), os.path.join(output_directory, "validation.pickle"))
-        self.process_data(os.path.join(input_directory, "test"), os.path.join(output_directory, "test.pickle"))
-        
+        for data_point in data:
+            tokens = data_point.document_tokens + data_point.query_tokens + data_point.index_to_entity.keys()
+            for token in tokens:
+                word_set.add(token)
+
+        for token in special_tokens:
+            word_set.add(token)
+
+        vocab = {}
+        for i, elem in enumerate(word_set):
+            vocab[elem] = i
+
+        return vocab
+
+
+    def replace_words(self, tokens):
+        ids = []
+        for token in tokens:
+            if token in self.word_to_id:
+                ids.append(self.word_to_id[token])
+            else:
+                ids.append(self.word_to_id["<unk>"])
+        return ids
+
+
+    def get_data_set(self, data_path):
+
+        data = pickle.load(open(data_path, "rb"))
+
+        documents = []
+        questions = []
+        answers = []
+        entities = []
+
+        for data_point in data:
+
+            documents.append(self.replace_words(data_point.document_tokens))
+            questions.append(self.replace_words(data_point.question_tokens))
+            answers.append(self.replace_words(data_point.answer_token))
+            entities.append(self.replace_words(data_point.entities))
+
+        return documents, questions, answers, entities
+
+    # def process_dataset(self, input_directory, output_directory):
+
+    #     self.process_data(os.path.join(input_directory, "training"), os.path.join(output_directory, "training.pickle"))
+    #     self.process_data(os.path.join(input_directory, "validation"), os.path.join(output_directory, "validation.pickle"))
+    #     self.process_data(os.path.join(input_directory, "test"), os.path.join(output_directory, "test.pickle"))
 
 
 
-test = DataLoader()
-test_dir = "/home/michiel/main/datasets/asreader/data/cnn/questions/"
-output_path = "/home/michiel/main/datasets/asreader/data/cnn/processed/"
-
-test.process_dataset(test_dir, output_path)
-
-with open("/home/michiel/main/datasets/asreader/data/cnn/processed/validation.pickle", "r") as fout:
-            a = pickle.load(fout)
-
-print(a[0].document_tokens)
