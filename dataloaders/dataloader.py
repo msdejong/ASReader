attempted softmax fix

dataloaders/dataloader.py
@@ -156,9 +156,9 @@ class DataLoader():
                 # We need to randomly shuffle the word_ids for entities every batch.
                 # We create a new random mapping from entity word ids to entity word ids and replace the entities
 
-                randomized_entities = self.randomize_entities(entity_vocabulary)
-                for data_point in batch_data:
-                    self.replace_entities(data_point, randomized_entities)
+                # randomized_entities = self.randomize_entities(entity_vocabulary)
+                # for data_point in batch_data:
+                #     self.replace_entities(data_point, randomized_entities)
 
                 batch_query_lengths = np.array([len(data_point.query_tokens) for data_point in batch_data])
 
@@ -180,6 +180,7 @@ class DataLoader():
                 answer_mask = np.array([[int(x == answers[i]) for x in documents[i]]
                                         for i in range(batch_length)])
 
+
                 # Create separate sort indices based on descending length for packing later
                 query_sort = np.argsort(batch_query_lengths)[::-1].copy()
                 document_sort = np.argsort(batch_document_lengths)[::-1].copy()
@@ -187,9 +188,14 @@ class DataLoader():
                 queries = queries[query_sort,...]
                 documents = documents[document_sort,...]
 
+
                 # Sort the lengths in the same order that the documents / queries are sorted
                 batch_query_lengths = batch_query_lengths[query_sort]
                 batch_document_lengths = batch_document_lengths[document_sort]
+
+                # Create length mask from sorted document, because it will be used in the model 
+                length_mask = np.array([[int(x < batch_document_lengths[i]) 
+                                         for x in range(maximum_document_length)] for i in range(batch_length)])
 
                 # Indices to reverse the sort
                 query_unsort = np.argsort(query_sort)
@@ -207,6 +213,7 @@ class DataLoader():
                 batch['doclengths'] = batch_document_lengths
                 batch['qlengths'] = batch_query_lengths
                 batch['ansmask'] = answer_mask
+                batch['lengthmask'] = length_mask
                 batch["entlocations"] = entity_locations
                 batch['docunsort'] = document_unsort
                 batch['qunsort'] = query_unsort

main.py
@@ -32,17 +32,20 @@ def evaluate_batches(model, eval_batches):
         batch_query_unsort = Variable(torch.LongTensor(batch['qunsort']))
         batch_document_unsort = Variable(torch.LongTensor(batch['docunsort']))
 
+        batch_length_mask = Variable(torch.FloatTensor(batch['lengthmask']).unsqueeze(-1))
+
         if USE_CUDA:
             batch_documents = batch_documents.cuda()
             batch_queries = batch_queries.cuda()
             batch_query_unsort = batch_query_unsort.cuda()
             batch_document_unsort = batch_document_unsort.cuda()
+            batch_length_mask = batch_length_mask.cuda()
             probs = model(batch_documents, batch_queries, batch_query_lengths,
-                          batch_doc_lengths, batch_query_unsort, batch_document_unsort).data.cpu().numpy()
+                          batch_doc_lengths, batch_query_unsort, batch_document_unsort, batch_length_mask).data.cpu().numpy()
 
         else:
             probs = model(batch_documents, batch_queries, batch_query_lengths,
-                          batch_doc_lengths, batch_query_unsort, batch_document_unsort).data.numpy()
+                          batch_doc_lengths, batch_query_unsort, batch_document_unsort, batch_length_mask).data.numpy()
 
         score += evaluate(probs, batch_entity_locations,
                           batch_doc_lengths[batch['docunsort']], batch_length) * batch_length
@@ -115,6 +118,8 @@ def train(model, training_data, dataloader, num_epochs=2, batch_size=32, bucket_
             batch_queries = Variable(torch.LongTensor(batch['queries']))
             # 1 for locations with the answer token and 0 elsewhere
             batch_answer_mask = Variable(torch.FloatTensor(batch['ansmask']).unsqueeze(-1))
+            batch_length_mask = Variable(torch.FloatTensor(batch['lengthmask']).unsqueeze(-1))
+
             # Similar to answer mask, but for every other entity
             batch_entity_locations = batch['entlocations']
             # Index to un- sort the arrays
@@ -125,19 +130,19 @@ def train(model, training_data, dataloader, num_epochs=2, batch_size=32, bucket_
                 batch_documents = batch_documents.cuda()
                 batch_queries = batch_queries.cuda()
                 batch_answer_mask = batch_answer_mask.cuda()
+                batch_length_mask = batch_length_mask.cuda()
                 batch_query_unsort = batch_query_unsort.cuda()
                 batch_document_unsort = batch_document_unsort.cuda()
 
             probs = model(batch_documents, batch_queries, batch_query_lengths,
-                          batch_doc_lengths, batch_query_unsort, batch_document_unsort)
-            loss = model.loss(probs, batch_answer_mask) / batch_length
-
+                          batch_doc_lengths, batch_query_unsort, batch_document_unsort, batch_length_mask)
+            loss = model.loss(probs, batch_answer_mask)
             loss.backward()
 
             torch.nn.utils.clip_grad_norm(model.parameters(), clip_threshold)
             optimizer.step()
 
-            # Calculate training loss and accuracy. 
+            # Calculate training loss and accuracy.
             # Multiplying by batch length and later dividing by sum of batch lengths to be correct for differing batch length at end of epoch
 
             if USE_CUDA:
@@ -195,6 +200,4 @@ if __name__ == "__main__":
 
     print("Starting training")
     train(model, training_data, DL, batch_size=args.batch_size, bucket_size=args.bucket_size,
-          learning_rate=args.learning_rate, valid_data=valid_data, eval_interval=args.eval_interval)
-
-
+          learning_rate=args.learning_rate, valid_data=valid_data, eval_interval=args.eval_interval, num_epochs=args.num_epochs)

models/ASReader.py
@@ -20,7 +20,7 @@ class ASReader(nn.Module):
                                      bidirectional=True, batch_first=True)
         self.softmax = nn.Softmax(dim=1)
 
-    def forward(self, document_batch, query_batch, query_lengths, document_lengths, query_unsort, document_unsort):
+    def forward(self, document_batch, query_batch, query_lengths, document_lengths, query_unsort, document_unsort, length_mask):
         
         query_embedded = self.embedding_layer(query_batch)
         document_embedded = self.embedding_layer(document_batch)
@@ -46,6 +46,11 @@ class ASReader(nn.Module):
         # Take the dot product of document encodings and the query encoding.
         scores = torch.bmm(document_unsorted, query_unsorted)
         probs = self.softmax(scores)
+
+        # Normalize properly by multiplying by ratio of sum including and excluding padded values
+        denom_ratio = (torch.sum(probs, dim=1) / torch.sum(probs * length_mask, dim=1)).squeeze()
+        probs = probs * denom_ratio 
+        
         return probs
 
     def loss(self, probs, answer_mask):
@@ -53,4 +58,4 @@ class ASReader(nn.Module):
         # Calculate the sum of probabilities over the positions in the document that have the answer token by multiplying all other positions by 0
         answer_probs = torch.sum(probs * answer_mask, 1)
         loss_vector = -torch.log(answer_probs)
-        return torch.sum(loss_vector)
+        return torch.mean(loss_vector)