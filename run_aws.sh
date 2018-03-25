3aeb49f15ad2a1b72777a8d1fe59ee0b787770b9
Michiel29 <michiel.s.dejong@gmail.com>
2018-03-24 21:07:41 -0400

    working

--- run_aws.sh
+++ run_aws.sh
@@ -1,4 +1,4 @@
-python main.py --train_path /home/ubuntu/drive/home/ubuntu/747/questions/training --valid_path /home/ubuntu/drive/home/ubuntu/747/questions/valid\
+python main.py --train_path /home/ubuntu/drive/home/ubuntu/747/questions/training --valid_path /home/ubuntu/drive/home/ubuntu/747/questions/validation \
 --test_path /home/ubuntu/drive/home/ubuntu/747/questions/test --eval_interval 500 --use_cuda True --num_epochs 2 --learning_rate 0.001
 
 
