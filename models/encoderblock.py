c5272891919de437b194ed3c279174133d61ece8
Michiel29 <michiel.s.dejong@gmail.com>
2018-04-24 19:58:02 -0400

    encoder block

--- models/encoderblock.py
+++ models/encoderblock.py
@@ -77,7 +77,7 @@ class ScaledDotProductAttention(nn.Module):
         attn = torch.bmm(queries, keys.transpose(1, 2)) / self.temper
         size = attn.size()
         repeated_mask = mask.repeat(self.number_heads, 1, size[2])
-        inverted_mask = (- repeated_mask.data + self.one_scalar).byte()
+        inverted_mask = (- repeated_mask.data + self.one_scalar.data).byte()
 
         attn.data.masked_fill_(inverted_mask, -10**10)
         attn = self.softmax(attn)
