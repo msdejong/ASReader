2a811b9e460e05f60cea8549b6354d325830c46e
Ubuntu <ubuntu@ip-172-31-11-214.ec2.internal>
2018-04-25 02:48:54 +0000

    profiled

--- run_aws.sh
+++ run_aws.sh
@@ -1,4 +1,4 @@
-python main.py --train_path /home/ubuntu/drive/home/ubuntu/747/questions/training --valid_path /home/ubuntu/drive/home/ubuntu/747/questions/validation \
---test_path /home/ubuntu/drive/home/ubuntu/747/questions/test --eval_interval 500 --use_cuda True --num_epochs 2 --learning_rate 0.001 --seed 2
+python -m cProfile -o attention.cprof  main.py --train_path /home/ubuntu/drive/home/ubuntu/747/questions/training --valid_path /home/ubuntu/drive/home/ubuntu/747/questions/validation \
+--test_path /home/ubuntu/drive/home/ubuntu/747/questions/test --eval_interval 500 --use_cuda True --num_epochs 2 --learning_rate 0.001 --seed 2 --batch_size 16
 
 
